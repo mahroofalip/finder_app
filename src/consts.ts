92aafb5ff63e4e2aef95db43eb42b1842753fc34
EXT-Mahroof <alimahroof155@gmail.com>
2024-10-06 23:02:25 +0530

    new server ip added

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
-export const URL  = ""
-export const intewellToFetch  = 60000
+export const URL = ""
+export const intewellToFetch = 60000
 export const backgroundNav = '#fff'
 export const orangeHeaderBg = 'orange'
 export const appname = "Kizzora"
-// export const BASE_URL = "http://13.201.115.71:5000"
-export const BASE_URL = "http://localhost:5000"
+export const BASE_URL = "http://3.110.193.3"
+// export const BASE_URL = "http://localhost:5000"
